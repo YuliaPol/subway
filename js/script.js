1f41f0c4f9cc91b144faeb958ffa083c880d97c8
YuliaPol <julia.polyeshko@gmail.com>
2021-03-31 10:16:59 +0300

    add company and resource page

--- js/script.js
+++ js/script.js
@@ -10,51 +10,6 @@ jQuery(function ($) {
             }
         });
 
-        $('#message-file').change(function(event){
-            if (window.File && window.FileList && window.FileReader) {
-                var files = event.target.files; //FileList object
-                if($(this).parents('.form-add-message').find('.download-files-wrapper').length == 0){
-                    var fileContHtml = '<div class="download-files-wrapper"></div>';
-                    $(fileContHtml).appendTo($(this).parents('.form-add-message'));
-                }
-                var filesCont = $(this).parents('.form-add-message').find('.download-files-wrapper');
-                filesCont.html(' ');
-                if(files.length == 0){
-                    filesCont.remove();
-                    return ;
-                }
-                for (var i = 0; i < files.length; i++) {
-                    var file = files[i];
-                    //Only pics
-                    console.log(file.name);
-                    if (file.type.match('image')){
-                        var picReader = new FileReader();
-                        picReader.addEventListener("load", function (event) {
-                            var picFile = event.target;
-                            let fileItemHtml = 
-                                '<div class="download-file-item">'
-                                +'    <div class="icon-file" style="background-image: url(' + picFile.result + ');"></div>'
-                                +'   <div class="file-name">'+ file.name + '</div>'
-                                +'</div>';
-                            $(fileItemHtml).appendTo(filesCont);
-                        });
-                        //Read the image
-                        picReader.readAsDataURL(file);
-                    }
-                    else {
-                        let fileItemHtml = 
-                            '<div class="download-file-item">'
-                            +'    <div class="icon-file"></div>'
-                            +'   <div class="file-name">'+ file.name + '</div>'
-                            +'</div>';
-                        $(fileItemHtml).appendTo(filesCont);
-                    }
-                }
-            } else {
-                console.log("Your browser does not support File API");
-            }
-        });
-
         $('.toggle-sidebar').click( function(){
             $('.sidebar').toggleClass('hide');
             $('.header').toggleClass('full-width');
@@ -87,14 +42,6 @@ jQuery(function ($) {
             return false;
         });
 
-        $('.tabs-wrapper .tab-item').click(function(e){
-            $(this).parents('.tabs-wrapper').find('.tab-item').removeClass('active');
-            $(this).addClass('active');
-            var openBlock = $(this).attr('data-open');
-            $(this).parents('.tabs-wrapper').find('.tab-content').fadeOut(0);
-            $('#' + openBlock).fadeIn(300);
-        });
-        
         $('.page-content').on('input', '.form-add-message textarea', function(e){
             this.style.height = 'auto';
             this.style.height = (this.scrollHeight) + 'px';
